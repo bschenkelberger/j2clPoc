5980e292d36a93cbaf6aab29549bc220d004432d
Bjoern Schenkelberger <schenkelberger@gmx.de>
2023-07-04 12:54:17 +0200

    Initial commit

---
+++ src/main/java/de/bs/j2cl/poc/UserValidation.java
@@ -0,0 +1,27 @@
+package de.bs.j2cl.poc;
+
+import org.treblereel.j2cl.processors.annotations.GWT3Export;
+import java.util.logging.Logger;
+
+public class UserValidation {
+
+  Logger logger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
+
+  @GWT3Export
+  public Boolean checkMinlengthFirstName(String name) {
+    Boolean result = true;
+    if (name.length() <= 3) {
+      result = false;
+    }
+    return result;
+  }
+ 
+  @GWT3Export
+  public Boolean validAge(User user) {
+    Boolean result = true;
+    if (user.getAge() < 18) {
+      result = false;
+    }
+    return result;
+  }
+}
